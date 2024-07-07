d36ac6b54b244a8ea63d9c639ea805b9b85134da
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2024-07-07 07:14:56 -0400

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
+const config: Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui],
+};
+
+export default config;
